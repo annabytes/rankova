eaeff21cd18f1ebf43112d1054e10c4c4cb228cb
Nazar <nick20name17@gmail.com>
2024-11-01 14:50:22 +0200

    init i18n

--- app/elements/lang-select/LangSelect.tsx
+++
@@ -1,8 +0,0 @@
-export const LangSelect = () => {
-    return (
-        <select name="lang" id="lang">
-            <option value="ukr">Укр</option>
-            <option value="en">Eng</option>
-        </select>
-    );
-};
